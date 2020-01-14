28fc1ade76cb7acadbf29d4d6445c0d727f541b2
Justin Hart <justinhart90@gmail.com>
2020-01-13 17:09:15 -0700

    MVP

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 import RandomNumber from './RandomNumber';
+import shuffle from 'lodash.shuffle';
 
 class Game extends React.Component {
     static propTypes = {
         randomNumberCount: PropTypes.number.isRequired,
+        initialSeconds: PropTypes.number.isRequired,
     };
 
     state = {
-        selectedNumbers: [],
+        selectedIds: [],
+        remainingSeconds: this.props.initialSeconds,
     };
 
     randomNumbers = Array
@@ -22,33 +25,97 @@ class Game extends React.Component {
         .slice(0, this.props.randomNumberCount - 2)
         .reduce((acc, curr) => acc + curr, 0);
 
+    shuffledRandomNumbers = shuffle(this.randomNumbers);
     // shuffle random numbers
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState((prevState) => {
+                return { remainingSeconds: prevState.remainingSeconds - 1 };
+            }, () => {
+                if (this.state.remainingSeconds === 0) {
+                    clearInterval(this.intervalId);
+                }
+            });
+        }, 1000);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+    };
     
-    isNumberSelected = (numberIndex) => this.state.selectedNumbers.indexOf(numberIndex) !== -1;
+    isNumberSelected = (numberIndex) => this.state.selectedIds.indexOf(numberIndex) !== -1;
 
     selectNumber = (numberIndex) => {
         this.setState((prevState) => ({
-            selectedNumbers: [...prevState.selectedNumbers, numberIndex],
+            selectedIds: [...prevState.selectedIds, numberIndex],
         }));
     };
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.target}>{this.target}</Text>
+    UNSAFE_componentWillUpdate(nextProps, nextState) {
+        if (
+            nextState.selectedIds !== this.state.selectedIds ||
+            nextState.remainingSeconds === 0
+        ) {
+            this.gameStatus = this.calcGameStatus(nextState);
 
-                <View style={styles.randomContainer}>
-                    {this.randomNumbers.map((randomNumber, index) => 
-                        <RandomNumber 
-                            key={index}
-                            id={index}
-                            number={randomNumber}
-                            isDisabled={this.isNumberSelected(index)}
-                            onPress={this.selectNumber}
-                        />
-                    )}
-                </View>
+            // if game has already finished, clear the timer
+            if (this.gameStatus !== 'PLAYING') {
+                clearInterval(this.intervalId);
+            }
+        }
+    };
+
+    gameStatus = 'PLAYING';
+
+    calcGameStatus = (nextState) => {
+        if (nextState.remainingSeconds === 0 && this.gameStatus === 'PLAYING') {
+            return 'LOST';
+        }
+
+        const sumSelected = nextState.selectedIds.reduce((acc, curr) => {
+            return acc + this.shuffledRandomNumbers[curr]
+        }, 0);
+        
+        if (sumSelected < this.target) {
+            return 'PLAYING';
+        }
+        
+        if (sumSelected === this.target) {
+            return 'WON';
+        }
+
+        return 'LOST';
+    };
+
+    targetPanelStyle = (gameStatus) => {
+
+    };
+
+    render() {
+        const gameStatus = this.gameStatus;
+        return (
+          <View style={styles.container}>
+            <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>
+              {this.target}
+            </Text>
+
+            <View style={styles.randomContainer}>
+              {this.shuffledRandomNumbers.map((randomNumber, index) => (
+                <RandomNumber
+                  key={index}
+                  id={index}
+                  number={randomNumber}
+                  isDisabled={
+                    this.isNumberSelected(index) || gameStatus !== 'PLAYING'
+                  }
+                  onPress={this.selectNumber}
+                />
+              ))}
             </View>
+            <Button title="Play Again" onPress={() => {}} />
+            <Text style={styles.target}>{this.state.remainingSeconds}</Text>
+          </View>
         );
     }
 }
@@ -62,10 +129,9 @@ const styles = StyleSheet.create({
     
     target: {
         fontSize: 50,
-        backgroundColor: 'blue',
-        color: 'white',
         margin: 50,
         textAlign: 'center',
+        color: 'white',
     },
 
     randomContainer: {
@@ -73,6 +139,18 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-around',
+    },
+
+    STATUS_PLAYING: {
+        backgroundColor: 'blue',
+    },
+    
+    STATUS_WON: {
+        backgroundColor: 'green',
+    },
+    
+    STATUS_LOST: {
+        backgroundColor: 'red',
     },
 });
 
